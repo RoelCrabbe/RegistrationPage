55232cdf327aa958ba2ad64b4d26bcee0786d3b9
RoelCrabbe <crabberoel@gmail.com>
2025-08-04 16:35:32 +0200

    update sample

--- config.sample.php
+++ config.sample.php
@@ -1,12 +1,22 @@
 <?php
 
-// Rename this file to config.php and fill in your database credentials.
+// Database credentials. Assuming you are running MySQL
+// server with default setting (user 'root' with no password)
 define('DB_SERVER', '127.0.0.1:3306');
 define('DB_USERNAME', 'your_db_user');
 define('DB_PASSWORD', 'your_db_pass');
 define('DB_NAME', 'realmd');
 
+// SOAP credentials and connection info (move here)
 define('SOAP_REGNAME', 'your_soap_user');
 define('SOAP_REGPASS', 'your_soap_pass');
 define('SOAP_HOST', '127.0.0.1');
 define('SOAP_PORT', 7878);
+
+// Attempt to connect to MySQL database
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if ($link === false) {
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
